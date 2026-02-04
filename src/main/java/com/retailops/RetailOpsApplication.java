f0eccad4e205e2dd525ce07d17a5b6eb3039f33b
Surya Srinivasan <surya@Suryas-MacBook-Air.local>
2026-02-05 03:29:48 +0530

    modified RetailOpsApplication.java

--- src/main/java/com/retailops/RetailOpsApplication.java
+++ src/main/java/com/retailops/RetailOpsApplication.java
@@ -2,10 +2,15 @@ package com.retailops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(
+        exclude = {
+                UserDetailsServiceAutoConfiguration.class
+        }
+)
 @EnableJpaRepositories(basePackages = "com.retailops.repository")
 @EntityScan(basePackages = "com.retailops.entity")
 public class RetailOpsApplication {
