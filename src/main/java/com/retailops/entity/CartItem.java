120155169a8d313e50cd6a129cbf09c09e4f6441
Surya Srinivasan <suryasrinivasan1306@gmail.com>
2026-03-01 21:35:48 +0530

    Update CartItem.java

--- src/main/java/com/retailops/entity/CartItem.java
+++ src/main/java/com/retailops/entity/CartItem.java
@@ -1,53 +1,50 @@
 package com.retailops.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "cart_items")
 public class CartItem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long userId;
-    private Long productId;
-    private Integer quantity;
+    private String username; // link cart to user
 
-    public CartItem() {
-    }
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    private int quantity;
+
+    public CartItem() {}
 
     public Long getId() {
         return id;
     }
- 
-    public void setId(Long id) {
-        this.id = id;
+
+    public String getUsername() {
+        return username;
     }
- 
-    public Long getUserId() {
-        return userId;
+
+    public void setUsername(String username) {
+        this.username = username;
     }
- 
-    public void setUserId(Long userId) {
-        this.userId = userId;
+
+    public Product getProduct() {
+        return product;
     }
- 
-    public Long getProductId() {
-        return productId;
+
+    public void setProduct(Product product) {
+        this.product = product;
     }
- 
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
- 
-    public Integer getQuantity() {
+
+    public int getQuantity() {
         return quantity;
     }
- 
-    public void setQuantity(Integer quantity) {
+
+    public void setQuantity(int quantity) {
         this.quantity = quantity;
     }
 }
