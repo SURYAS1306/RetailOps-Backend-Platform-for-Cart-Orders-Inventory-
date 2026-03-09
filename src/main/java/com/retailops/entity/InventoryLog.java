07441e6fb64ee8acc18f56e73368939a1cb38be8
Surya Srinivasan <suryasrinivasan1306@gmail.com>
2026-03-09 19:59:53 +0530

    Create InventoryLog.java

---
+++ src/main/java/com/retailops/entity/InventoryLog.java
@@ -0,0 +1,60 @@
+package com.retailops.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "inventory_logs")
+public class InventoryLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productName;
+
+    private int quantityChanged;
+
+    private String action; // STOCK_DECREASE / STOCK_INCREASE
+
+    private LocalDateTime timestamp;
+
+    public InventoryLog() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantityChanged() {
+        return quantityChanged;
+    }
+
+    public void setQuantityChanged(int quantityChanged) {
+        this.quantityChanged = quantityChanged;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+}
